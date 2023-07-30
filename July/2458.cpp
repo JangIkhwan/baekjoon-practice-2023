267e4d42349fcda847ed0b74d38a2750605b10d5
JangIkhwan <bluensky0213@gmail.com>
2023-07-30 22:21:37 +0900

    230730 학습 내용

---
+++ July/2458.cpp
@@ -0,0 +1,65 @@
+﻿/*
+23-07-30 21:18 ~ 22:03
+
+1차 시도 : 성공
+
+- 그래프 상에서 해당 노드로 도달할 수 있는 노드의 수와 
+해당 노드에서 시작해서 도달할 수 있는 노드의 수가 N - 1개면 
+순위를 알 수 있음
+
+생각해볼 점
+- 플로이드 워셜로 풀 수 있을까?
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+int N, M;
+vector<int> graph[501];
+bool visited[501];
+
+int lowCount[501];
+int highCount[501];
+
+void bfs(int start) {
+	queue<int> qu;
+	qu.push(start);
+	visited[start] = true;
+	while (!qu.empty()) {
+		int now = qu.front();
+		qu.pop();
+		for (int next : graph[now]) {
+			if (!visited[next]) {
+				qu.push(next);
+				visited[next] = true;
+				lowCount[next] += 1;
+				highCount[start] += 1;
+			}
+		}
+	}
+}
+
+int main(void) {
+	int a, b;
+	int result;
+	scanf("%d %d", &N, &M);
+	for (int i = 1; i <= M; i++) {
+		scanf("%d %d", &a, &b);
+		graph[a].push_back(b);
+	}
+	for (int start = 1; start <= N; start++) {
+		bfs(start);
+		memset(visited, 0, sizeof(visited));
+	}
+	result = 0;
+	for (int node = 1; node <= N; node++) {
+		if (lowCount[node] + highCount[node] == N - 1) {
+			result += 1;
+		}
+	}
+	printf("%d\n", result);
+	return 0;
+}
---
+++ July/4963.cpp
@@ -0,0 +1,63 @@
+﻿/*
+23-07-30 20:48 ~ 21:06
+
+1차 시도 : 성공
+
+bfs를 이용해서 섬을 하나씩 찾아가면 됨
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+int map[51][51];
+bool visited[51][51];
+int dy[] = { 0, 1, 1, 1, 0, - 1, -1, -1 };
+int dx[] = { -1, -1, 0, 1, 1, 1, 0, -1 };
+
+void bfs(int y, int x, int w, int h) {
+	queue<pair<int, int>> qu;
+	qu.push({ y, x });
+	visited[y][x] = true;
+	while (!qu.empty()) {
+		int y = qu.front().first;
+		int x = qu.front().second;
+		qu.pop();
+		for (int direct = 0; direct < 8; direct++) {
+			int nextY = y + dy[direct];
+			int nextX = x + dx[direct];
+			if (nextY < 1 || nextY > h || nextX < 1 || nextX > w) continue;
+			if (map[nextY][nextX] == 1 && !visited[nextY][nextX]) { //
+				qu.push({ nextY, nextX });
+				visited[nextY][nextX] = true; // 
+			}
+		}
+	}
+}
+
+int main(void) {
+	int w, h;
+	while (true) {
+		scanf("%d %d", &w, &h);
+		if (w == 0 && h == 0) break;
+		for (int y = 1; y <= h; y++) {
+			for (int x = 1; x <= w; x++) {
+				scanf("%d", &map[y][x]);
+			}
+		}
+		memset(visited, 0, sizeof(visited)); //
+		int islandCount = 0;
+		for (int y = 1; y <= h; y++) {
+			for (int x = 1; x <= w; x++) {
+				if (map[y][x] == 1 && !visited[y][x]) {
+					bfs(y, x, w, h);
+					islandCount++;
+				}
+			}
+		}
+		printf("%d\n", islandCount);
+	}
+	return 0;
+}
+
